a84cc679e8229c5774dd0a0900026dcd52ce9bff
Kiril Panayotov <31936449+zakrok@users.noreply.github.com>
2026-02-20 03:10:45 +0100

    Crush astro (#11)
    * genesis-2
    
    * genesis-2
    
    * genesis-3
    
    ---------
    
    Co-authored-by: zakrok <31936449+Zakrok09@users.noreply.github.com>

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import Artt from "$lib/Artt.svelte";
+</script>
+
+<Artt />
+
+<main class="w-screen h-screen p-8 pb-20 flex flex-col-reverse">
+    <div class="flex flex-col">
+        <span class="text-gray-500 text-lg font-pixel">
+             # intern @ jetbrains & computer science student @ TUDelft
+        </span>
+        <h1 class="text-gray-100 font-bold text-4xl font-pixel">
+            // kiril panayotov
+        </h1>
+        <hr class="p-4">
+        <div class="flex gap-4">
+            <a href="projects" class="bg-white hover:bg-gray-300 px-2 self-start text-black font-pixel cursor-pointer">
+                // explore my work
+            </a>
+            <a href="blog" class="text-white hover:text-gray-300 px-2 self-start font-pixel cursor-pointer">
+                # browse my blog
+            </a>
+            <a href="about" class="text-white hover:text-gray-300 px-2 self-start font-pixel cursor-pointer">
+                # learn more about me
+            </a>
+        </div>
+        <div class="flex gap-4 mt-2">
+            <a href="https://github.com/zakrok"
+               class="text-white hover:text-gray-300 px-2 self-start font-pixel cursor-pointer underline"
+               target="_blank">
+                GitHub
+            </a>
+            <a href="https://www.linkedin.com/in/kiril-panayotov/"
+               class="text-white hover:text-gray-300 px-2 self-start font-pixel cursor-pointer underline"
+               target="_blank">
+                LinkedIn
+            </a>
+        </div>
+    </div>
+</main>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,9 @@
+
+<nav class="font-pixel text-white w-1/2 mx-auto mb-12 mt-6">
+    <a href="/" class="underline">home</a>
+    <a href="/projects" class="underline bg-white text-black">projects</a>
+    <a href="/blog" class="underline">blog</a>
+    <a href="/about" class="underline">about</a>
+</nav>
+
+<p class="font-pixel text-white w-1/2 mx-auto">work in progress</p>
