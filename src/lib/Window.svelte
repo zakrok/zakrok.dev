a868147f0d31819ddff1970231c53bb457ea5fce
zakrok <31936449+Zakrok09@users.noreply.github.com>
2026-02-21 06:49:41 +0100

    feat: windows and dedsec

---
+++ src/lib/Window.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let { title, bottom, children }: { title: string, bottom: number, children: any } = $props();
+</script>
+
+<aside
+    class="hidden md:block border-2 w-52 border-white font-pixel text-white bg-black"
+    style="right: 2em; bottom: {bottom}em">
+    <div class="flex justify-between gap-8 px-2 border-b-white border-b-2">
+        <span>{title}</span>
+        <div class="flex gap-2">
+            <span>_</span><span>[]</span><span>X</span>
+        </div>
+    </div>
+    <div class="p-4">
+        {@render children()}
+    </div>
+</aside>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,49 @@
-<script>
+<script lang="ts">
     import Artt from "$lib/Artt.svelte";
+    import Window from "$lib/Window.svelte";
+    import {onMount} from "svelte";
+
+    async function copy_88() {
+        try {
+            await navigator.clipboard.writeText(
+                "<a href=\"https://zakrok.dev\" target=\"_blank\">\n" +
+                "    <img src=\"https://zakrok.dev/88x31.gif\" alt=\"zakrok.dev personal website banner\" />\n" +
+                "</a>\n"
+            )
+        } catch (e) {}
+    }
+
+    let audio: HTMLAudioElement;
+
+    onMount(() => {
+        audio.autoplay = true
+        audio.volume = 0.3
+    })
 </script>
 
 <Artt />
+
+<div class="absolute flex flex-col gap-2" style="bottom: 2em; right: 2em;">
+    <Window title="d3ds3c.ogg" bottom={8.5}>
+        <audio bind:this={audio} src="/watch_dogs_2.ogg" type="audio/ogg" autoplay loop>
+            browser cannot play audio
+        </audio>
+        <button on:click={() => audio.play()} class="cursor-pointer underline">play</button>
+        <button on:click={() => audio.pause()} class="cursor-pointer underline">un/pause</button>
+        <button on:click={() => audio.volume += 0.1} class="cursor-pointer text-2xl">+</button>
+        <button on:click={() => audio.volume -= 0.1} class="cursor-pointer text-2xl">-</button>
+        <span><i class="text-sm">keep in mind this song is not made by me and it is from Watch Dogs 2</i></span>
+    </Window>
+    <Window title="shmls_shill.txt" bottom={8.5}>
+        <h3># mention me</h3>
+        <button on:click={copy_88} class="cursor-pointer">
+            <img src="/88x31.gif" alt="Click to copy me!" />
+        </button>
+    </Window>
+    <Window title="my_friends.txt" bottom={2}>
+        <h3>imagine having those</h3>
+    </Window>
+</div>
 
 <main class="w-screen h-screen p-8 pb-20 flex flex-col-reverse">
     <div class="flex flex-col">
