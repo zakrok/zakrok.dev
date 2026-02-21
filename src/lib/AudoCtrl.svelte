211665bb1b61e50eba63555a1ea9b66c651f8b53
zakrok <31936449+Zakrok09@users.noreply.github.com>
2026-02-21 19:28:04 +0100

    feat: a bunch

---
+++ src/lib/AudoCtrl.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    let audio: HTMLAudioElement;
+
+    onMount(() => {
+        audio.autoplay = true
+        audio.volume = 0.3
+    })
+</script>
+
+<div class="flex flex-col">
+    <div class="flex gap-4">
+        <audio bind:this={audio} src="/watch_dogs_2.ogg" autoplay loop>
+            browser cannot play audio
+        </audio>
+        <button on:click={() => audio.play()} class="cursor-pointer underline">play</button>
+        <button on:click={() => audio.pause()} class="cursor-pointer underline">un/pause</button>
+        <button on:click={() => audio.volume += 0.1} class="cursor-pointer text-2xl">+</button>
+        <button on:click={() => audio.volume -= 0.1} class="cursor-pointer text-2xl">-</button>
+    </div>
+
+    <i class="text-sm text-gray-300 w-48 leading-4">keep in mind this song is not made by me and it is from Watch Dogs 2</i>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,48 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+    <title>zakrok.dev</title>
+    <meta name="author" content="Praxidike">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="personal website of kiril panayotov. explore my projects and blog">
+    <meta name="og:type" content="website">
+    <meta name="og:title" content="zakrok.dev">
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="msapplication-TileImage" content="/favicon-144x144.png">
+    <meta name="msapplication-config" content="/browserconfig.xml">
+
+    <meta property="og:url"                 content="https://zakrok.dev/" />
+    <meta property="og:site_name"           content="zakrok.dev" />
+    <meta property="og:image"               content="https://zakrok.dev/ogmeta.png">
+    <meta property="og:image:alt"           content="zakrok.dev">
+    <meta property="og:image:width"         content="1200">
+    <meta property="og:image:height"        content="630">
+    <meta property="og:locale"              content="en_US" />
+
+    <meta name="twitter:card" content="summary_large_image">
+
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="lang" content="en">
+</svelte:head>
 {@render children()}
